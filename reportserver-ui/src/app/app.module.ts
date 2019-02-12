0574d5be9c4ab718b38518c0024efceb6fb2fc31
Robin <robin.kurovszky@gmail.com>
2019-02-12 16:15:16 +0100

    Add dep for material

--- reportserver-ui/src/app/app.module.ts
+++ reportserver-ui/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MatInputModule, MatFormFieldModule, MatSelectModule, MatOptionModule, MatCardModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
